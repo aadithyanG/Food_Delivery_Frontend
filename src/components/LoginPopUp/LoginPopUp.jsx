94061b278db5ee164347a83549aff8e0123a6357
aadithyanG <aadithyanganesan@gmail.com>
2025-03-03 18:57:36 +0530

    Initial commit

--- src/components/LoginPopUp/LoginPopUp.jsx
+++ src/components/LoginPopUp/LoginPopUp.jsx
@@ -1,22 +1,54 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './LoginPopUp.css'
 import { assets } from '../../assets/assets'
 const LoginPopUp = ({setShowLogin}) => {
   
-  const [currState,setCurrState]=useState("Login")
+  const [currState,setCurrState]=useState("Login");
+  const [formData,setFormData]=useState({
+    name:'',//Store name when signing up 
+    email:'',
+    password:'',
+  })
+
+  
+
+  const handleChange=(e)=>{
+    setFormData({
+      ...formData,
+      [e.target.name]:e.target.value,
+    });
+  };
+  
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log(formData)
+   
+    // 1. Get users from localStorage
+    const storedUsers= localStorage.getItem('users');
+    const users = storedUsers ? JSON.parse(storedUsers):``;
+
+    const existingUser=users.find(user=>user.email === formData.email && user.password === formData.password);
+    if(existingUser){
+      console.log('Login Succesful!');
+    }else{
+      console.error("Invalid email or password");
+    }
+    localStorage.setItem('users',JSON.stringify([...users,{email:formData.email,password:formData.password}]));
+
+  };
   
   return (
     <div className='login-popup'>
-      <form  className="login-popup-container">
+      <form  className="login-popup-container" onSubmit={handleSubmit}>
         <div className="login-popup-title">
           <h2>{currState}</h2>
           <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
         </div>
         <div className="login-popup-inputs">
           
-          {currState==="Login"?<></>:<input type="text" placeholder='Your Name' required />}
-          <input type="email"  placeholder='Your Email' required/>
-          <input type="password" placeholder='Password' required />
+          {currState==="Login"?<></>:<input type="text" placeholder='Your Name' name="name" value={formData.name} onChange={handleChange}required />}
+          <input type="email"  placeholder='Your Email' name="email" value={formData.email} onChange={handleChange} required/>
+          <input type="password" placeholder='Password' name="password" value={formData.password} onChange={handleChange} required />
         </div>
         <button>
           {currState==="Sign Up"?"Create account":"Login"}
